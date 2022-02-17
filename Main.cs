32f6a89c3f5fcc41c366857946e12dbcb25eeb7c
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-02-17 20:03:18 +0300

    Main
    Main

---
+++ Main.cs
@@ -0,0 +1,87 @@
+ void main()
+        {
+            while (true)
+            {
+                swed.GetProcess(HEQCKTJQO.Text);
+                CheckModule = swed.GetModuleBase(".exe");
+                var cbcbmhandle= swed.ReadPointer(CheckModule, entity.CbTostring);
+                var HandeCheatBlocker = swed.ReadPointer(CheckModule, entity.CheatBlockergg);
+                var ABCBlocker = swed.ReadPointer(CheckModule, entity.ABS);
+                if (namesp.Checked)
+                {
+                    var handleesp = swed.ReadPointer(CheckModule, entity.NameAll);
+                    swed.WriteBytes(handleesp, isimler.N1,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N2,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N3,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N4,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N5,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N6,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N7,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N8,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N9,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N10,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N11,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N12,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N13,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N14,
+                        BitConverter.GetBytes(256));
+                    swed.WriteBytes(handleesp, isimler.N15,
+                        BitConverter.GetBytes(256));
+                    Thread.Sleep(1);
+
+                    var WallHack= swed.ReadPointer(CheckModule, entity.Wallhacktr);
+                    swed.WriteBytes(handleesp, isimler.N1, BitConverter.GetBytes(Bypass.BypassMethot));
+                }
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F1 , BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F2, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9)); // readlıyorum
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F3, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F4, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F5, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F6, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F7, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F8, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F9, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F10, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F11, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F12, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F13, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F14, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F15, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F16, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F17, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F18, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F19, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F20, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F21, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F22, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F23, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F24, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(HandeCheatBlocker, CbCbm.F25, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                HideBypass();
+                CheatBlockerBypass();
+                CbDumping();
+                swed.WriteBytes(ABCBlocker, ABC.A1, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A2, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A3, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A4, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A5, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A6, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A7, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A8, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A9, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+                swed.WriteBytes(ABCBlocker, ABC.A10, BitConverter.GetBytes(Bypass.BypassMethot + Bypass.SendMethot + Bypass.RandomString + 0x9));
+            }
+        }
