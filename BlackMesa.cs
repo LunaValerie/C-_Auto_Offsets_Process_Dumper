b330e8859de7afbdfa13fcd4c12f0064e4e5335d
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-02-18 22:23:10 +0300

    Offsets
    Offsets

---
+++ BlackMesa.cs
@@ -0,0 +1,17 @@
+LocalPlayer : "server.dll"+0083449C ( 0xE4 )
+ViewMatrix : engine.dll+3F316C
+Armour : 0xD98
+TeamNum : 0x1F4
+NameEsp : 0xED4
+Health : 0xE0
+Dormant : 0x60
+X : 0x218
+Z : 0x21C
+XYZ : 24C
+Entity : 0x0D
+EntityList : 0x1D8
+Fov : 0x0B8C
+ViewMode : 0x48C
+Velocity: 0x28C
+Glow : 0x6B4
+Weapon : 0x0D
